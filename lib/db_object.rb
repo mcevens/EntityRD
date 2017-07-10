a7179b7448682faa1583d9b20b836a5589d8e015
Mc Evens <mcevens.bienaime@gmail.com>
2017-07-10 07:54:06 -0400

    Add columns method to list all columns

--- lib/db_object.rb
+++ lib/db_object.rb
@@ -3,6 +3,20 @@ require 'active_support/inflector'
 
 class DBObject
 
+  def self.columns
+  return @columns if @columns
+  cols = DBConnection.execute2(<<-SQL).first
+    SELECT
+      *
+    FROM
+      #{self.table_name}
+    LIMIT
+      0
+  SQL
+  cols.map!(&:to_sym)
+  @columns = cols
+end
+
   def self.table_name=(table_name)
     @table_name = table_name
   end
@@ -10,5 +24,19 @@ class DBObject
   def self.table_name
     @table_name || self.name.underscore.pluralize
   end
+
+  def self.columns
+   return @columns if @columns
+   cols = DBConnection.execute2(<<-SQL).first
+     SELECT
+       *
+     FROM
+       #{self.table_name}
+     LIMIT
+       0
+   SQL
+   cols.map!(&:to_sym)
+   @columns = cols
+ end
 
 end
