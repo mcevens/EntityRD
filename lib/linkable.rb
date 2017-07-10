a99007ac2348cefbe59bc0903b05e1a6e8e61c01
Mc Evens <mcevens.bienaime@gmail.com>
2017-07-10 08:37:34 -0400

    Create LinkOptions

--- lib/linkable.rb
+++ lib/linkable.rb
@@ -1,7 +1,19 @@
 require_relative 'filterable'
 
+class LinkOptions
+  attr_accessor :foreign_key, :class_name, :primary_key
+
+  def class_object
+    @class_name.constantize
+  end
+
+  def table_name
+    class_object.table_name
+  end
+end
+
 module Linkable
-  
+
 end
 
 class SQLObject
