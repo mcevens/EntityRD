aede3b7a08b4f57712bcb7ae19e020e149d6ac18
Mc Evens <mcevens.bienaime@gmail.com>
2017-07-10 07:58:07 -0400

    Add finalize method to add getters and setters

--- lib/db_object.rb
+++ lib/db_object.rb
@@ -3,20 +3,6 @@ require 'active_support/inflector'
 
 class DBObject
 
-  def self.columns
-  return @columns if @columns
-  cols = DBConnection.execute2(<<-SQL).first
-    SELECT
-      *
-    FROM
-      #{self.table_name}
-    LIMIT
-      0
-  SQL
-  cols.map!(&:to_sym)
-  @columns = cols
-end
-
   def self.table_name=(table_name)
     @table_name = table_name
   end
@@ -38,5 +24,17 @@ end
    cols.map!(&:to_sym)
    @columns = cols
  end
+
+ def self.finalize!
+  self.columns.each do |name|
+    define_method(name) do
+      self.attributes[name]
+    end
+
+    define_method("#{name}=") do |value|
+      self.attributes[name] = value
+    end
+  end
+end
 
 end
