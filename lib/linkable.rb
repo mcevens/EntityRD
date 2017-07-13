e5206cfad1a8d9680493cf06fd055e64ed81d2e8
Mc Evens <mcevens.bienaime@gmail.com>
2017-07-12 22:17:28 -0400

    Add BelongsToOptions

--- lib/linkable.rb
+++ lib/linkable.rb
@@ -12,6 +12,20 @@ class LinkOptions
   end
 end
 
+class BelongsToOptions < LinkOptions
+  def initialize(name, options = {})
+  defaults = {
+    :foreign_key => "#{name}_id".to_sym,
+    :class_name => name.to_s.camelcase,
+    :primary_key => :id
+  }
+
+  defaults.keys.each do |key|
+    self.send("#{key}=", options[key] || defaults[key])
+  end
+end
+end
+
 module Linkable
 
 end
