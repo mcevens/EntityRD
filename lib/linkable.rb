f2b60a69cfa502a0e6a8f7d89747f80aae2ba9ce
Mc Evens <mcevens.bienaime@gmail.com>
2017-07-12 22:18:43 -0400

    Create HasManyOptions

--- lib/linkable.rb
+++ lib/linkable.rb
@@ -26,6 +26,20 @@ class BelongsToOptions < LinkOptions
 end
 end
 
+class HasManyOptions < AssocOptions
+  def initialize(name, self_class_name, options = {})
+    defaults = {
+      :foreign_key => "#{self_class_name.underscore}_id".to_sym,
+      :class_name => name.to_s.singularize.camelcase,
+      :primary_key => :id
+    }
+
+    defaults.keys.each do |key|
+      self.send("#{key}=", options[key] || defaults[key])
+    end
+  end
+end
+
 module Linkable
 
 end
